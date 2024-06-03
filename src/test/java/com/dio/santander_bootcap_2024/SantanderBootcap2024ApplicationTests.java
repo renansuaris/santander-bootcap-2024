c408f31ced983d2c15ab2b0db985af2574362426
Renan Soares <renanalencar313@gmail.com>
2024-06-03 11:17:56 -0300

    First Commit - Upload project to GitHub

---
+++ src/test/java/com/dio/santander_bootcap_2024/SantanderBootcap2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander_bootcap_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcap2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
